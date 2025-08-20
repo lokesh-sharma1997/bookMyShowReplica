6a3b4c4f79250b6b1aabd28765aea989f302cb90
Priyanshu Saini <Priyanshu.Saini@in2ittech.com>
2025-08-20 19:28:02 +0530

    Created movie api

---
+++ src/main/java/com/bookmyshow/main/repository/MovieRepository.java
@@ -0,0 +1,11 @@
+package com.bookmyshow.main.repository;
+
+
+
+import com.bookmyshow.main.model.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+    Optional<Movie> findByName(String name);
+}
